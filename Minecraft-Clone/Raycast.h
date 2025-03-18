e833ee52c70c3e3604b762069131b02033915858
E-Dawkins <ethandawkins@hotmail.co.uk>
2025-03-18 12:26:59 +1100

    Added hitNormal to raycast HitResult

--- Minecraft-Clone/Raycast.h
+++ Minecraft-Clone/Raycast.h
@@ -6,11 +6,13 @@
 struct HitResult {
 	BlockType hitType = BlockType::AIR;
 	glm::ivec3 hitPos = { 0, 0, 0 };
+	glm::ivec3 hitNormal = { 0 ,0, 0 };
 
 public:
 	std::string to_string() {
 		return "HitResult: " + std::string(BlockNames[hitType + 1]) + " | " +
-			std::to_string(hitPos.x) + "," + std::to_string(hitPos.y) + "," + std::to_string(hitPos.z);
+			"p: " + std::to_string(hitPos.x) + "," + std::to_string(hitPos.y) + "," + std::to_string(hitPos.z) + " | " +
+			"n: " + std::to_string(hitNormal.x) + "," + std::to_string(hitNormal.y) + "," + std::to_string(hitNormal.z);
 	}
 };
 
@@ -33,6 +35,21 @@ public:
 			distanceTravelled += rayMarchDistance;
 		}
 
-		return { ChunkManager::getInstance()->getBlockAtPos(gridPos), gridPos };
+		glm::vec3 hitPoint = rayStart + rayDirection * distanceTravelled;
+		glm::vec3 dirToPt = glm::normalize(hitPoint - glm::vec3(gridPos));
+
+		float largestDot = 0.f;
+		glm::vec3 closestNormal = { 0, 0, 0 };
+
+		for (uint8_t i = 0; i < BlockFace::FACE_COUNT; i++) {
+			glm::vec3 currentNormal = faceNormals[i];
+			float d = glm::dot(dirToPt, currentNormal);
+			if (d > largestDot) {
+				largestDot = d;
+				closestNormal = currentNormal;
+			}
+		}
+
+		return { ChunkManager::getInstance()->getBlockAtPos(gridPos), gridPos, closestNormal };
 	}
 };
